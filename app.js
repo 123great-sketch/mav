013c0ccb651aa617bcc288a6f98d42b60ac80335
123great-sketch <greatnessadebayo39@gmail.com>
2025-09-28 23:10:20 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+const video1 =  document.getElementById("projectVideo1");
+const video2 =  document.getElementById("projectVideo2");
+const video3 =  document.getElementById("projectVideo3");
+const hoversign = document.querySelector(".hover-sign");
+
+const videoList = [video1, video2, video3];
+
+videoList.forEach(function(video){
+    video.addEventListener("mouseover", function(){
+        video.play()
+        hoversign.classList.add("active")
+    })
+    video.addEventListener("mouseout", function(){
+        video.pause()
+        hoversign.classList.remove("active")
+    })
+})
+
